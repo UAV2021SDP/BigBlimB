2017a81ff22f26583967e2448a4b1f6b9f355e94
Danielle Arianna Laganiere <dlaganie@ucsc.edu>
2022-04-06 19:09:18 -0700

    Cleaned up motor control & feedback

--- GPIO.py
+++ GPIO.py
@@ -4,10 +4,10 @@ import time
 # acting as a pseudo #define
 # motor states as it circles the encoder
 MotorStates = {
-    "State00" : 1,
-    "State10" : 2,
-    "State11" : 3,
-    "State01" : 4
+    "State00" : 0b00,
+    "State10" : 0b10,
+    "State11" : 0b11,
+    "State01" : 0b01
 }
 
 #motor direction
@@ -24,13 +24,18 @@ GPIO.setup(12, GPIO.OUT)    #deploy when positive
 GPIO.setup(32, GPIO.OUT)    #retract when positive
 deployMotor = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz   
 retractMotor = GPIO.PWM(32, 50)  # channel=32 frequency=50Hz   
+#motor hall sensor inputs
+GPIO.setup(36, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)   #A
+GPIO.setup(38, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)   #B
 
 class Motor:
-    def __init__(self, state, count=0):
+    def __init__(self, state=0b00, count=0):
         self.count = count
         self.state = state
     def increaseCount(self):
         self.count = self.count + 1
+    def setCount(self, newcount):
+        self.count = newcount
     def decreaseCount(self):
         self.count = self.count - 1
     def setState(self, newstates: MotorStates):
@@ -61,12 +66,19 @@ def RunMotor(motor: Motor, newAB):
             motor.increaseCount
     motor.setState(newAB)
 
+def getNewAB():
+    A = GPIO.input(36)
+    B = GPIO.input(38)
+    return ((A<<1)+B)
+
 def RunMotorByLength(motor: Motor, direction, turn_amount):
     # info about PWM: https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
     #PWM_Pin.start(dc)                  # start PWM where dc is the duty cycle (0.0 <= dc <= 100.0)
     #PWM_Pin.ChangeFrequency(freq)      # change frequency where freq is the new frequency in Hz
     #PWM_Pin.ChangeDutyCycle(dc)        # change duty cycle where 0.0 <= dc <= 100.0
     #PWM_Pin.stop()                     # stop PWM
+
+    motor = Motor()
 
     if direction == Direction["deploy"]:
         ControllingMotor = deployMotor
@@ -78,23 +90,45 @@ def RunMotorByLength(motor: Motor, direction, turn_amount):
 
     #needs to ease the start to prevent jarring
     ControllingMotor.start(25)
-    time.sleep(0.2)
+    while(motor.count < 100):
+        newAB = getNewAB()
+        RunMotor(motor, newAB)
     ControllingMotor.ChangeDutyCycle(50)
-    time.sleep(0.2)
+    while(motor.count < 200):
+        newAB = getNewAB()
+        RunMotor(motor, newAB)
     ControllingMotor.ChangeDutyCycle(75)
-    time.sleep(0.2)
+    while(motor.count < 300):
+        newAB = getNewAB()
+        RunMotor(motor, newAB)
+        #reset count to restart count
+
+    #actually deploy or return
+    motor.setCount(0)
     ControllingMotor.ChangeDutyCycle(100)
+    while(motor.count < turn_amount):
+        newAB = getNewAB()
+        RunMotor(motor, newAB)
+        #reset count to restart count
 
-    #let the motor move for the determined time
-    time.sleep(turn_amount)
-
+    
     #needs to ease the start to prevent jarring
     ControllingMotor.ChangeDutyCycle(100)
-    time.sleep(0.2)
+    while(motor.count < 100):
+        newAB = getNewAB()
+        RunMotor(motor, newAB)
     ControllingMotor.ChangeDutyCycle(75)
-    time.sleep(0.2)
+    while(motor.count < 200):
+        newAB = getNewAB()
+        RunMotor(motor, newAB)
     ControllingMotor.ChangeDutyCycle(50)
-    time.sleep(0.2)
+    while(motor.count < 300):
+        newAB = getNewAB()
+        RunMotor(motor, newAB)
     ControllingMotor.ChangeDutyCycle(25)
-    time.sleep(0.2)
+    while(motor.count < 400):
+        newAB = getNewAB()
+        RunMotor(motor, newAB)
     ControllingMotor.stop()
+
+    return motor.count, motor.state
